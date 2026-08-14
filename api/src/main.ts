9c440ec8d5938058051d4a479ec6005296734f9e
Vadym <vadym.kotenko.dev@gmail.com>
2026-08-14 13:46:31 +0300

    feat: configure global pipes, filters, cors, and config service

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { GlobalExceptionFilter } from './shared/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { type AppConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const corsOrigin =
+    configService.get<string>('app.cors') || 'http://localhost:5173';
+
+  app.enableCors({
+    origin: corsOrigin.split(','),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const appConfiguration = configService.get<AppConfig>('app');
+  if (!appConfiguration) {
+    throw new Error('App configuration is missing');
+  }
+  app.useGlobalFilters(new GlobalExceptionFilter(appConfiguration));
+
+  const port = configService.get<number>('app.port') || 3000;
+  await app.listen(port);
 }
 void bootstrap();
