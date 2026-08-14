d6a87d398c36e102b76c0f576e1d4289e88796a0
Vadym <vadym.kotenko.dev@gmail.com>
2026-08-14 10:54:55 +0300

    feat: wire up application module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,22 +1,41 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { awsConfig } from './config/aws.config';
 import { appConfigSchema } from './config/config.types';
 import { databaseConfig } from './config/database.config';
 import { DocumentsModule } from './modules/documents/documents.module';
+import { appConfig } from './config/app.config';
+import { DatabaseModule } from './modules/database/database.module';
+import { SearchModule } from './modules/search/search.module';
+import { SseModule } from './modules/sse/sse.module';
+import { SqsWorkerModule } from './modules/sqs-worker/sqs-worker.module';
+import { StorageModule } from './modules/storage/storage.module';
+import { GlobalExceptionFilter } from './shared/filters/http-exception.filter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [databaseConfig, awsConfig],
+      load: [databaseConfig, awsConfig, appConfig],
       validationSchema: appConfigSchema,
     }),
+    DatabaseModule,
     DocumentsModule,
+    StorageModule,
+    SearchModule,
+    SseModule,
+    SqsWorkerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
